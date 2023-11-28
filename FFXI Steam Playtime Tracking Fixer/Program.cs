20d09ef21bc1da85e0b9ac000f1746438fcdc9e0
Sebastian Scaini <sebastianscaini@gmail.com>
2023-11-28 12:10:48 -0500

    Created Application

---
+++ FFXI Steam Playtime Tracking Fixer/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.Mime.MediaTypeNames;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("FFXI Steam Playtime Tracking Fixer by Sebastian Scaini");
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Launching FFXI through Steam.");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Keep this window open while the game is running for Steam to track your playtime!");
+//Console.WriteLine(AppContext.BaseDirectory + @"SquareEnix\PlayOnlineViewer\pol.exe");
+System.Diagnostics.Process.Start(AppContext.BaseDirectory + @"SquareEnix\PlayOnlineViewer\pol.exe");
+Console.ReadKey();
+Console.ForegroundColor = ConsoleColor.White;
+Environment.Exit(0);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FFXI-Steam-Playtime-Tracking-Fixer
+## Installation Instructions
+1. Extract all zipped files into your FFXINA folder.
+1.1 You can get to this folder easily from Steam by right clicking on FFXI and selecting "Browse Local Files".
+2. Set the launch option to: "PATH TO YOUR GAME INSTALL HERE\FFXINA\FFXI Steam Playtime Tracking Fixer.exe" %command%
+replacing "PATH TO YOUR GAME INSTALL HERE" with the path to your FFXINA folder. 
+2.1 The default path is "C:\Program Files (x86)\Steam\steamapps\common\FFXINA\FFXI Steam Playtime Tracking Fixer.exe" but if you have it on a different drive you will need to adjust the path.
+3. Press play on Steam and keep the console application window open. Steam will recognize that window as FFXI and track the playtime while it remains open.
