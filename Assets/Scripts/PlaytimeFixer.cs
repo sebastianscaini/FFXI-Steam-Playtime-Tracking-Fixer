060a94425c47fdd597201850967be8b7b8e85f91
Sebastian Scaini <sebastianscaini@gmail.com>
2023-12-27 19:00:21 -0500

    Created Launching Functionality

--- Assets/Scripts/PlaytimeFixer.cs
+++ Assets/Scripts/PlaytimeFixer.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 
 public class PlaytimeFixer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        System.Diagnostics.Process.Start(Application.dataPath + @"SquareEnix\PlayOnlineViewer\pol.exe");
+        //Debug.Log(Application.dataPath + @"\SquareEnix\PlayOnlineViewer\pol.exe");
+        Process p = new Process();
+        p.StartInfo.FileName = System.IO.Directory.GetParent(Application.dataPath) + @"\SquareEnix\PlayOnlineViewer\pol.exe";
+        p.Start();
+        //System.Diagnostics.Process.Start(Application.dataPath + @"\SquareEnix\PlayOnlineViewer\pol.exe");
     }
 }
